762ff9b92b18dbfc276e79e7b9a4e39d6fa4be46
AzisBasyari <azisbasyari3010@gmail.com>
2022-04-21 13:50:35 +0700

    initial commit

---
+++ database/migrations/2022_04_21_053153_create_fasilitas_kamars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fasilitas_kamars', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kamar_id');
+            $table->foreign('kamar_id')->references('id')->on('kamars')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('nama_fasilitas');
+            $table->string('deskripsi');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fasilitas_kamars');
+    }
+};
